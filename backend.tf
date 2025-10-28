10963ee0f2a3d2e3bbb390f753d17f41aec5bfca
helge <helge.nubes@gmail.com>
2025-10-28 17:34:41 +0200

    feat: add backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "crime-bucker"                 # Ваше имя bucket
+    key            = "terraform.tfstate"            # Путь к state файлу
+    region         = "eu-north-1"                    # Ваш регион
+    use_lockfile   = true
+    encrypt        = true
+  }
+}
