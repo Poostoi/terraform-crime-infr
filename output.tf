135d462aad2332d55264bcff68991d2ceea8c15f
helge <helge.nubes@gmail.com>
2025-10-28 18:49:34 +0200

    fix: format terrform files

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 output "instance_info" {
- description = "Адресcа машин"
- value = [
-  for vm in aws_instance.servers : {
+  description = "Адресcа машин"
+  value = [
+    for vm in aws_instance.servers : {
       name = vm.tags.Name,
-      dns = vm.public_dns,
-      ip = vm.public_ip
+      dns  = vm.public_dns,
+      ip   = vm.public_ip
     }
-  ] 
+  ]
 }
--- provider.tf
+++ provider.tf
@@ -5,10 +5,10 @@ provider "aws" {
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.92"
     }
-  } 
+  }
 
   required_version = ">= 1.2"
 }
